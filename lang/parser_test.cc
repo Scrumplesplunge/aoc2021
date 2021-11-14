a524269b5f08e570253280f87b107cb6522e3f39
Joe Fowler <scrumplesplunge@gmail.com>
2021-11-14 10:18:22 +0000

    Use a test fixture to remove some boilerplate.

--- lang/parser_test.cc
+++ lang/parser_test.cc
@@ -6,257 +6,229 @@
 namespace aoc2021 {
 namespace {
 
-TEST(Name) {
-  Source source("test_input.aoc", "x");
-  EXPECT_EQ(Parser(source).ParseExpression(),
-            ast::Name(Seek(source, 1, 1).value(), "x"));
+struct ParserTest : Test {
+  void WithSource(std::string_view contents) {
+    source.emplace("test_input.aoc", contents);
+  }
+
+  Location At(int line, int column) const {
+    return Seek(source.value(), line, column).value();
+  }
+
+  ast::Expression ParseExpression() const {
+    Parser parser(source.value());
+    ast::Expression result = parser.ParseExpression();
+    parser.ExpectEnd();
+    return result;
+  }
+
+  std::optional<Source> source;
+};
+
+TEST_F(ParserTest, Name) {
+  WithSource("x");
+  EXPECT_EQ(ParseExpression(), ast::Name(At(1, 1), "x"));
 }
 
-TEST(IntegerLiteral) {
-  Source source("test_input.aoc", "123");
-  EXPECT_EQ(Parser(source).ParseExpression(),
-            ast::IntegerLiteral(Seek(source, 1, 1).value(), 123));
+TEST_F(ParserTest, IntegerLiteral) {
+  WithSource("123");
+  EXPECT_EQ(ParseExpression(), ast::IntegerLiteral(At(1, 1), 123));
 }
 
-TEST(ZeroPaddingIsDisallowed) {
-  Source source("test_input.aoc", "0123");
-  EXPECT_ERROR(Parser(source).ParseExpression(),
-               "integer literals must not be 0-padded");
+TEST_F(ParserTest, ZeroPaddingIsDisallowed) {
+  WithSource("0123");
+  EXPECT_ERROR(ParseExpression(), "integer literals must not be 0-padded");
 }
 
-TEST(TrailingCharactersDisallowed) {
-  Source source("test_input.aoc", "123foo");
-  EXPECT_ERROR(Parser(source).ParseExpression(), "invalid integer literal");
+TEST_F(ParserTest, TrailingCharactersDisallowed) {
+  WithSource("123foo");
+  EXPECT_ERROR(ParseExpression(), "invalid integer literal");
 }
 
-TEST(MissingBracket) {
-  Source source("test_input.aoc", "(1 + 2");
-  EXPECT_ERROR(Parser(source).ParseExpression(), "expected ')'");
+TEST_F(ParserTest, MissingBracket) {
+  WithSource("(1 + 2");
+  EXPECT_ERROR(ParseExpression(), "expected ')'");
 }
 
-TEST(Call) {
-  Source source("test_input.aoc", "f(1, x)");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
+TEST_F(ParserTest, Call) {
+  WithSource("f(1, x)");
+  EXPECT_EQ(ParseExpression(), ast::Call(At(1, 2), ast::Name(At(1, 1), "f"),
+                                         {ast::IntegerLiteral(At(1, 3), 1),
+                                          ast::Name(At(1, 6), "x")}));
+}
+
+TEST_F(ParserTest, CallMissingBracket) {
+  WithSource("f(1, x");
+  EXPECT_ERROR(ParseExpression(), "expected ')'");
+}
+
+TEST_F(ParserTest, Index) {
+  WithSource("a[b]");
+  EXPECT_EQ(ParseExpression(), ast::Index(At(1, 2), ast::Name(At(1, 1), "a"),
+                                          ast::Name(At(1, 3), "b")));
+}
+
+TEST_F(ParserTest, Prefix) {
+  WithSource(
+      "f(\n"
+      "-x,\n"
+      "!x,\n"
+      "~x,\n"
+      "*x)");
+  EXPECT_EQ(ParseExpression(),
+            ast::Call(At(1, 2), ast::Name(At(1, 1), "f"),
+                      {ast::Negate(At(2, 1), ast::Name(At(2, 2), "x")),
+                       ast::LogicalNot(At(3, 1), ast::Name(At(3, 2), "x")),
+                       ast::BitwiseNot(At(4, 1), ast::Name(At(4, 2), "x")),
+                       ast::Dereference(At(5, 1), ast::Name(At(5, 2), "x"))}));
+}
+
+TEST_F(ParserTest, Product) {
+  WithSource("a * b / c % d");
   EXPECT_EQ(
-      Parser(source).ParseExpression(),
-      ast::Call(at(1, 2), ast::Name(at(1, 1), "f"),
-                {ast::IntegerLiteral(at(1, 3), 1), ast::Name(at(1, 6), "x")}));
+      ParseExpression(),
+      ast::Modulo(At(1, 11),
+                  ast::Divide(At(1, 7),
+                              ast::Multiply(At(1, 3), ast::Name(At(1, 1), "a"),
+                                            ast::Name(At(1, 5), "b")),
+                              ast::Name(At(1, 9), "c")),
+                  ast::Name(At(1, 13), "d")));
 }
 
-TEST(CallMissingBracket) {
-  Source source("test_input.aoc", "f(1, x");
-  EXPECT_ERROR(Parser(source).ParseExpression(), "expected ')'");
-}
-
-TEST(Index) {
-  Source source("test_input.aoc", "a[b]");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
+TEST_F(ParserTest, Sum) {
+  WithSource("1 + 2 * 3 - 4");
   EXPECT_EQ(
-      Parser(source).ParseExpression(),
-      ast::Index(at(1, 2), ast::Name(at(1, 1), "a"), ast::Name(at(1, 3), "b")));
-}
-
-TEST(Prefix) {
-  Source source("test_input.aoc",
-                "f(\n"
-                "-x,\n"
-                "!x,\n"
-                "~x,\n"
-                "*x)");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
-  EXPECT_EQ(Parser(source).ParseExpression(),
-            ast::Call(at(1, 2), ast::Name(at(1, 1), "f"),
-                      {ast::Negate(at(2, 1), ast::Name(at(2, 2), "x")),
-                       ast::LogicalNot(at(3, 1), ast::Name(at(3, 2), "x")),
-                       ast::BitwiseNot(at(4, 1), ast::Name(at(4, 2), "x")),
-                       ast::Dereference(at(5, 1), ast::Name(at(5, 2), "x"))}));
-}
-
-TEST(Product) {
-  Source source("test_input.aoc", "a * b / c % d");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
-  EXPECT_EQ(
-      Parser(source).ParseExpression(),
-      ast::Modulo(at(1, 11),
-                  ast::Divide(at(1, 7),
-                              ast::Multiply(at(1, 3), ast::Name(at(1, 1), "a"),
-                                            ast::Name(at(1, 5), "b")),
-                              ast::Name(at(1, 9), "c")),
-                  ast::Name(at(1, 13), "d")));
-}
-
-TEST(Sum) {
-  Source source("test_input.aoc", "1 + 2 * 3 - 4");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
-  EXPECT_EQ(
-      Parser(source).ParseExpression(),
+      ParseExpression(),
       ast::Subtract(
-          at(1, 11),
-          ast::Add(at(1, 3), ast::IntegerLiteral(at(1, 1), 1),
-                   ast::Multiply(at(1, 7), ast::IntegerLiteral(at(1, 5), 2),
-                                 ast::IntegerLiteral(at(1, 9), 3))),
-          ast::IntegerLiteral(at(1, 13), 4)));
+          At(1, 11),
+          ast::Add(At(1, 3), ast::IntegerLiteral(At(1, 1), 1),
+                   ast::Multiply(At(1, 7), ast::IntegerLiteral(At(1, 5), 2),
+                                 ast::IntegerLiteral(At(1, 9), 3))),
+          ast::IntegerLiteral(At(1, 13), 4)));
 }
 
-TEST(Shift) {
-  Source source("test_input.aoc", "a << b + c >> d");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
-  EXPECT_EQ(Parser(source).ParseExpression(),
+TEST_F(ParserTest, Shift) {
+  WithSource("a << b + c >> d");
+  EXPECT_EQ(ParseExpression(),
             ast::ShiftRight(
-                at(1, 12),
-                ast::ShiftLeft(at(1, 3), ast::Name(at(1, 1), "a"),
-                               ast::Add(at(1, 8), ast::Name(at(1, 6), "b"),
-                                        ast::Name(at(1, 10), "c"))),
-                ast::Name(at(1, 15), "d")));
+                At(1, 12),
+                ast::ShiftLeft(At(1, 3), ast::Name(At(1, 1), "a"),
+                               ast::Add(At(1, 8), ast::Name(At(1, 6), "b"),
+                                        ast::Name(At(1, 10), "c"))),
+                ast::Name(At(1, 15), "d")));
 }
 
-TEST(Order) {
-  Source source("test_input.aoc",
-                "a\n"
-                "< b << c\n"
-                "<= d\n"
-                "> e\n"
-                ">= f");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
-  EXPECT_EQ(Parser(source).ParseExpression(),
+TEST_F(ParserTest, Order) {
+  WithSource(
+      "a\n"
+      "< b << c\n"
+      "<= d\n"
+      "> e\n"
+      ">= f");
+  EXPECT_EQ(ParseExpression(),
             ast::GreaterOrEqual(
-                at(5, 1),
+                At(5, 1),
                 ast::GreaterThan(
-                    at(4, 1),
+                    At(4, 1),
                     ast::LessOrEqual(
-                        at(3, 1),
+                        At(3, 1),
                         ast::LessThan(
-                            at(2, 1), ast::Name(at(1, 1), "a"),
-                            ast::ShiftLeft(at(2, 5), ast::Name(at(2, 3), "b"),
-                                           ast::Name(at(2, 8), "c"))),
-                        ast::Name(at(3, 4), "d")),
-                    ast::Name(at(4, 3), "e")),
-                ast::Name(at(5, 4), "f")));
+                            At(2, 1), ast::Name(At(1, 1), "a"),
+                            ast::ShiftLeft(At(2, 5), ast::Name(At(2, 3), "b"),
+                                           ast::Name(At(2, 8), "c"))),
+                        ast::Name(At(3, 4), "d")),
+                    ast::Name(At(4, 3), "e")),
+                ast::Name(At(5, 4), "f")));
 }
 
-TEST(Equal) {
-  Source source("test_input.aoc",
-                "1\n"
-                "== 2 < 3\n"
-                "!= 4");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
+TEST_F(ParserTest, Equal) {
+  WithSource(
+      "1\n"
+      "== 2 < 3\n"
+      "!= 4");
   EXPECT_EQ(
-      Parser(source).ParseExpression(),
+      ParseExpression(),
       ast::NotEqual(
-          at(3, 1),
-          ast::Equal(at(2, 1), ast::IntegerLiteral(at(1, 1), 1),
-                     ast::LessThan(at(2, 6), ast::IntegerLiteral(at(2, 4), 2),
-                                   ast::IntegerLiteral(at(2, 8), 3))),
-          ast::IntegerLiteral(at(3, 4), 4)));
+          At(3, 1),
+          ast::Equal(At(2, 1), ast::IntegerLiteral(At(1, 1), 1),
+                     ast::LessThan(At(2, 6), ast::IntegerLiteral(At(2, 4), 2),
+                                   ast::IntegerLiteral(At(2, 8), 3))),
+          ast::IntegerLiteral(At(3, 4), 4)));
 }
 
-TEST(BitwiseAnd) {
-  Source source("test_input.aoc", "a & b == c & d");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
-  EXPECT_EQ(Parser(source).ParseExpression(),
+TEST_F(ParserTest, BitwiseAnd) {
+  WithSource("a & b == c & d");
+  EXPECT_EQ(ParseExpression(),
             ast::BitwiseAnd(
-                at(1, 12),
-                ast::BitwiseAnd(at(1, 3), ast::Name(at(1, 1), "a"),
-                                ast::Equal(at(1, 7), ast::Name(at(1, 5), "b"),
-                                           ast::Name(at(1, 10), "c"))),
-                ast::Name(at(1, 14), "d")));
+                At(1, 12),
+                ast::BitwiseAnd(At(1, 3), ast::Name(At(1, 1), "a"),
+                                ast::Equal(At(1, 7), ast::Name(At(1, 5), "b"),
+                                           ast::Name(At(1, 10), "c"))),
+                ast::Name(At(1, 14), "d")));
 }
 
-TEST(BitwiseXor) {
-  Source source("test_input.aoc", "a ^ b & c ^ d");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
+TEST_F(ParserTest, BitwiseXor) {
+  WithSource("a ^ b & c ^ d");
   EXPECT_EQ(
-      Parser(source).ParseExpression(),
+      ParseExpression(),
       ast::BitwiseXor(
-          at(1, 11),
-          ast::BitwiseXor(at(1, 3), ast::Name(at(1, 1), "a"),
-                          ast::BitwiseAnd(at(1, 7), ast::Name(at(1, 5), "b"),
-                                          ast::Name(at(1, 9), "c"))),
-          ast::Name(at(1, 13), "d")));
+          At(1, 11),
+          ast::BitwiseXor(At(1, 3), ast::Name(At(1, 1), "a"),
+                          ast::BitwiseAnd(At(1, 7), ast::Name(At(1, 5), "b"),
+                                          ast::Name(At(1, 9), "c"))),
+          ast::Name(At(1, 13), "d")));
 }
 
-TEST(BitwiseOr) {
-  Source source("test_input.aoc", "a | b ^ c | d");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
+TEST_F(ParserTest, BitwiseOr) {
+  WithSource("a | b ^ c | d");
   EXPECT_EQ(
-      Parser(source).ParseExpression(),
+      ParseExpression(),
       ast::BitwiseOr(
-          at(1, 11),
-          ast::BitwiseOr(at(1, 3), ast::Name(at(1, 1), "a"),
-                          ast::BitwiseXor(at(1, 7), ast::Name(at(1, 5), "b"),
-                                          ast::Name(at(1, 9), "c"))),
-          ast::Name(at(1, 13), "d")));
+          At(1, 11),
+          ast::BitwiseOr(At(1, 3), ast::Name(At(1, 1), "a"),
+                         ast::BitwiseXor(At(1, 7), ast::Name(At(1, 5), "b"),
+                                         ast::Name(At(1, 9), "c"))),
+          ast::Name(At(1, 13), "d")));
 }
 
-TEST(Conjunction) {
-  Source source("test_input.aoc", "a && b | c && d");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
+TEST_F(ParserTest, Conjunction) {
+  WithSource("a && b | c && d");
   EXPECT_EQ(
-      Parser(source).ParseExpression(),
+      ParseExpression(),
       ast::LogicalAnd(
-          at(1, 12),
-          ast::LogicalAnd(at(1, 3), ast::Name(at(1, 1), "a"),
-                          ast::BitwiseOr(at(1, 8), ast::Name(at(1, 6), "b"),
-                                         ast::Name(at(1, 10), "c"))),
-          ast::Name(at(1, 15), "d")));
+          At(1, 12),
+          ast::LogicalAnd(At(1, 3), ast::Name(At(1, 1), "a"),
+                          ast::BitwiseOr(At(1, 8), ast::Name(At(1, 6), "b"),
+                                         ast::Name(At(1, 10), "c"))),
+          ast::Name(At(1, 15), "d")));
 }
 
-TEST(Disjunction) {
-  Source source("test_input.aoc", "a || b && c || d");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
+TEST_F(ParserTest, Disjunction) {
+  WithSource("a || b && c || d");
   EXPECT_EQ(
-      Parser(source).ParseExpression(),
+      ParseExpression(),
       ast::LogicalOr(
-          at(1, 13),
-          ast::LogicalOr(at(1, 3), ast::Name(at(1, 1), "a"),
-                         ast::LogicalAnd(at(1, 8), ast::Name(at(1, 6), "b"),
-                                         ast::Name(at(1, 11), "c"))),
-          ast::Name(at(1, 16), "d")));
+          At(1, 13),
+          ast::LogicalOr(At(1, 3), ast::Name(At(1, 1), "a"),
+                         ast::LogicalAnd(At(1, 8), ast::Name(At(1, 6), "b"),
+                                         ast::Name(At(1, 11), "c"))),
+          ast::Name(At(1, 16), "d")));
 }
 
-TEST(Ternary) {
-  Source source("test_input.aoc",
-                "a ?\n"
-                "b ? c : d :\n"
-                "e ? f : g");
-  auto at = [&](int line, int column) {
-    return Seek(source, line, column).value();
-  };
-  EXPECT_EQ(Parser(source).ParseExpression(),
+TEST_F(ParserTest, Ternary) {
+  WithSource(
+      "a ?\n"
+      "b ? c : d :\n"
+      "e ? f : g");
+  EXPECT_EQ(ParseExpression(),
             ast::TernaryExpression(
-                at(1, 3), ast::Name(at(1, 1), "a"),
-                ast::TernaryExpression(at(2, 3), ast::Name(at(2, 1), "b"),
-                                       ast::Name(at(2, 5), "c"),
-                                       ast::Name(at(2, 9), "d")),
-                ast::TernaryExpression(at(3, 3), ast::Name(at(3, 1), "e"),
-                                       ast::Name(at(3, 5), "f"),
-                                       ast::Name(at(3, 9), "g"))));
+                At(1, 3), ast::Name(At(1, 1), "a"),
+                ast::TernaryExpression(At(2, 3), ast::Name(At(2, 1), "b"),
+                                       ast::Name(At(2, 5), "c"),
+                                       ast::Name(At(2, 9), "d")),
+                ast::TernaryExpression(At(3, 3), ast::Name(At(3, 1), "e"),
+                                       ast::Name(At(3, 5), "f"),
+                                       ast::Name(At(3, 9), "g"))));
 }
 
 }  // namespace
