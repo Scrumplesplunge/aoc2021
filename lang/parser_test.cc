1494ae0c3d04f49662bdf2f6885b2c9fa2d88632
Joe Fowler <scrumplesplunge@gmail.com>
2021-11-14 11:23:24 +0000

    Rewrite tests to use more granular expectations.

--- lang/parser_test.cc
+++ lang/parser_test.cc
@@ -69,166 +69,258 @@ TEST_F(ParserTest, Index) {
 }
 
 TEST_F(ParserTest, Prefix) {
-  WithSource(
-      "f(\n"
-      "-x,\n"
-      "!x,\n"
-      "~x,\n"
-      "*x)");
+  WithSource("-x");
+  EXPECT_EQ(ParseExpression(), ast::Negate(At(1, 1), ast::Name(At(1, 2), "x")));
+  WithSource("!x");
   EXPECT_EQ(ParseExpression(),
-            ast::Call(At(1, 2), ast::Name(At(1, 1), "f"),
-                      {ast::Negate(At(2, 1), ast::Name(At(2, 2), "x")),
-                       ast::LogicalNot(At(3, 1), ast::Name(At(3, 2), "x")),
-                       ast::BitwiseNot(At(4, 1), ast::Name(At(4, 2), "x")),
-                       ast::Dereference(At(5, 1), ast::Name(At(5, 2), "x"))}));
+            ast::LogicalNot(At(1, 1), ast::Name(At(1, 2), "x")));
+  WithSource("~x");
+  EXPECT_EQ(ParseExpression(),
+            ast::BitwiseNot(At(1, 1), ast::Name(At(1, 2), "x")));
+  WithSource("*x");
+  EXPECT_EQ(ParseExpression(),
+            ast::Dereference(At(1, 1), ast::Name(At(1, 2), "x")));
+  // Test that precedence is handled correctly: suffix expressions (such as the
+  // brackets for a function call) should bind more tightly, and so -f() should
+  // negate the result of a function call rather than calling the result of
+  // a negation.
+  WithSource("-f()");
+  EXPECT_EQ(
+      ParseExpression(),
+      ast::Negate(At(1, 1), ast::Call(At(1, 3), ast::Name(At(1, 2), "f"), {})));
 }
 
 TEST_F(ParserTest, Product) {
-  WithSource("a * b / c % d");
+  WithSource("a * b");
+  EXPECT_EQ(ParseExpression(), ast::Multiply(At(1, 3), ast::Name(At(1, 1), "a"),
+                                             ast::Name(At(1, 5), "b")));
+  WithSource("a / b");
+  EXPECT_EQ(ParseExpression(), ast::Divide(At(1, 3), ast::Name(At(1, 1), "a"),
+                                           ast::Name(At(1, 5), "b")));
+  WithSource("a % b");
+  EXPECT_EQ(ParseExpression(), ast::Modulo(At(1, 3), ast::Name(At(1, 1), "a"),
+                                           ast::Name(At(1, 5), "b")));
+  // Test that expressions are left-associative: `a / b / c` should be
+  // `(a / b) / c` rather than `a / (b / c)`.
+  WithSource("a / b / c");
+  EXPECT_EQ(ParseExpression(),
+            ast::Divide(At(1, 7),
+                        ast::Divide(At(1, 3), ast::Name(At(1, 1), "a"),
+                                    ast::Name(At(1, 5), "b")),
+                        ast::Name(At(1, 9), "c")));
+  // Test that precedence is handled correctly: unary expressions should bind
+  // more tightly.
+  WithSource("-a * b");
   EXPECT_EQ(
       ParseExpression(),
-      ast::Modulo(At(1, 11),
-                  ast::Divide(At(1, 7),
-                              ast::Multiply(At(1, 3), ast::Name(At(1, 1), "a"),
-                                            ast::Name(At(1, 5), "b")),
-                              ast::Name(At(1, 9), "c")),
-                  ast::Name(At(1, 13), "d")));
+      ast::Multiply(At(1, 4), ast::Negate(At(1, 1), ast::Name(At(1, 2), "a")),
+                    ast::Name(At(1, 6), "b")));
 }
 
 TEST_F(ParserTest, Sum) {
-  WithSource("1 + 2 * 3 - 4");
-  EXPECT_EQ(
-      ParseExpression(),
-      ast::Subtract(
-          At(1, 11),
-          ast::Add(At(1, 3), ast::IntegerLiteral(At(1, 1), 1),
-                   ast::Multiply(At(1, 7), ast::IntegerLiteral(At(1, 5), 2),
-                                 ast::IntegerLiteral(At(1, 9), 3))),
-          ast::IntegerLiteral(At(1, 13), 4)));
+  WithSource("a + b");
+  EXPECT_EQ(ParseExpression(), ast::Add(At(1, 3), ast::Name(At(1, 1), "a"),
+                                        ast::Name(At(1, 5), "b")));
+  WithSource("a - b");
+  EXPECT_EQ(ParseExpression(), ast::Subtract(At(1, 3), ast::Name(At(1, 1), "a"),
+                                             ast::Name(At(1, 5), "b")));
+  // Test that expressions are left-associative: `a + b - c` should be
+  // `(a + b) - c` rather than `a + (b - c)`.
+  WithSource("a + b - c");
+  EXPECT_EQ(ParseExpression(),
+            ast::Subtract(At(1, 7),
+                          ast::Add(At(1, 3), ast::Name(At(1, 1), "a"),
+                                   ast::Name(At(1, 5), "b")),
+                          ast::Name(At(1, 9), "c")));
+  // Test that precedence is handled correctly: product expressions should bind
+  // more tightly.
+  WithSource("a + b * c");
+  EXPECT_EQ(ParseExpression(),
+            ast::Add(At(1, 3), ast::Name(At(1, 1), "a"),
+                     ast::Multiply(At(1, 7), ast::Name(At(1, 5), "b"),
+                                   ast::Name(At(1, 9), "c"))));
 }
 
 TEST_F(ParserTest, Shift) {
-  WithSource("a << b + c >> d");
+  WithSource("a << b");
   EXPECT_EQ(ParseExpression(),
-            ast::ShiftRight(
-                At(1, 12),
-                ast::ShiftLeft(At(1, 3), ast::Name(At(1, 1), "a"),
-                               ast::Add(At(1, 8), ast::Name(At(1, 6), "b"),
-                                        ast::Name(At(1, 10), "c"))),
-                ast::Name(At(1, 15), "d")));
+            ast::ShiftLeft(At(1, 3), ast::Name(At(1, 1), "a"),
+                           ast::Name(At(1, 6), "b")));
+  WithSource("a >> b");
+  EXPECT_EQ(ParseExpression(),
+            ast::ShiftRight(At(1, 3), ast::Name(At(1, 1), "a"),
+                            ast::Name(At(1, 6), "b")));
+  // Test that expressions are left-associative: `a << b << c` should be
+  // `(a << b) << c` rather than `a << (b << c)`.
+  WithSource("a << b << c");
+  EXPECT_EQ(ParseExpression(),
+            ast::ShiftLeft(At(1, 8),
+                           ast::ShiftLeft(At(1, 3), ast::Name(At(1, 1), "a"),
+                                          ast::Name(At(1, 6), "b")),
+                           ast::Name(At(1, 11), "c")));
+  // Test that precedence is handled correctly: sum expressions should bind
+  // more tightly.
+  WithSource("a << b + c");
+  EXPECT_EQ(ParseExpression(),
+            ast::ShiftLeft(At(1, 3), ast::Name(At(1, 1), "a"),
+                           ast::Add(At(1, 8), ast::Name(At(1, 6), "b"),
+                                    ast::Name(At(1, 10), "c"))));
 }
 
 TEST_F(ParserTest, Order) {
-  WithSource(
-      "a\n"
-      "< b << c\n"
-      "<= d\n"
-      "> e\n"
-      ">= f");
+  WithSource("a < b");
+  EXPECT_EQ(ParseExpression(), ast::LessThan(At(1, 3), ast::Name(At(1, 1), "a"),
+                                             ast::Name(At(1, 5), "b")));
+  WithSource("a <= b");
   EXPECT_EQ(ParseExpression(),
-            ast::GreaterOrEqual(
-                At(5, 1),
-                ast::GreaterThan(
-                    At(4, 1),
-                    ast::LessOrEqual(
-                        At(3, 1),
-                        ast::LessThan(
-                            At(2, 1), ast::Name(At(1, 1), "a"),
-                            ast::ShiftLeft(At(2, 5), ast::Name(At(2, 3), "b"),
-                                           ast::Name(At(2, 8), "c"))),
-                        ast::Name(At(3, 4), "d")),
-                    ast::Name(At(4, 3), "e")),
-                ast::Name(At(5, 4), "f")));
+            ast::LessOrEqual(At(1, 3), ast::Name(At(1, 1), "a"),
+                             ast::Name(At(1, 6), "b")));
+  WithSource("a > b");
+  EXPECT_EQ(ParseExpression(),
+            ast::GreaterThan(At(1, 3), ast::Name(At(1, 1), "a"),
+                             ast::Name(At(1, 5), "b")));
+  WithSource("a >= b");
+  EXPECT_EQ(ParseExpression(),
+            ast::GreaterOrEqual(At(1, 3), ast::Name(At(1, 1), "a"),
+                                ast::Name(At(1, 6), "b")));
+  // Test that expressions are left-associative: `a < b < c` should be
+  // `(a < b) < c` rather than `a < (b < c)`.
+  WithSource("a < b < c");
+  EXPECT_EQ(ParseExpression(),
+            ast::LessThan(At(1, 7),
+                          ast::LessThan(At(1, 3), ast::Name(At(1, 1), "a"),
+                                        ast::Name(At(1, 5), "b")),
+                          ast::Name(At(1, 9), "c")));
+  // Test that precedence is handled correctly: shift expressions should bind
+  // more tightly.
+  WithSource("a < b << c");
+  EXPECT_EQ(ParseExpression(),
+            ast::LessThan(At(1, 3), ast::Name(At(1, 1), "a"),
+                          ast::ShiftLeft(At(1, 7), ast::Name(At(1, 5), "b"),
+                                         ast::Name(At(1, 10), "c"))));
 }
 
 TEST_F(ParserTest, Equal) {
-  WithSource(
-      "1\n"
-      "== 2 < 3\n"
-      "!= 4");
-  EXPECT_EQ(
-      ParseExpression(),
-      ast::NotEqual(
-          At(3, 1),
-          ast::Equal(At(2, 1), ast::IntegerLiteral(At(1, 1), 1),
-                     ast::LessThan(At(2, 6), ast::IntegerLiteral(At(2, 4), 2),
-                                   ast::IntegerLiteral(At(2, 8), 3))),
-          ast::IntegerLiteral(At(3, 4), 4)));
+  WithSource("a == b");
+  EXPECT_EQ(ParseExpression(), ast::Equal(At(1, 3), ast::Name(At(1, 1), "a"),
+                                          ast::Name(At(1, 6), "b")));
+  WithSource("a != b");
+  EXPECT_EQ(ParseExpression(), ast::NotEqual(At(1, 3), ast::Name(At(1, 1), "a"),
+                                             ast::Name(At(1, 6), "b")));
+  // Test that expressions are left-associative: `a == b == c` should be
+  // `(a == b) == c` rather than `a == (b == c)`.
+  WithSource("a == b == c");
+  EXPECT_EQ(ParseExpression(),
+            ast::Equal(At(1, 8),
+                       ast::Equal(At(1, 3), ast::Name(At(1, 1), "a"),
+                                  ast::Name(At(1, 6), "b")),
+                       ast::Name(At(1, 11), "c")));
+  // Test that precedence is handled correctly: order expressions should bind
+  // more tightly.
+  WithSource("a == b < c");
+  EXPECT_EQ(ParseExpression(),
+            ast::Equal(At(1, 3), ast::Name(At(1, 1), "a"),
+                       ast::LessThan(At(1, 8), ast::Name(At(1, 6), "b"),
+                                     ast::Name(At(1, 10), "c"))));
 }
 
 TEST_F(ParserTest, BitwiseAnd) {
-  WithSource("a & b == c & d");
+  WithSource("a & b");
   EXPECT_EQ(ParseExpression(),
-            ast::BitwiseAnd(
-                At(1, 12),
-                ast::BitwiseAnd(At(1, 3), ast::Name(At(1, 1), "a"),
-                                ast::Equal(At(1, 7), ast::Name(At(1, 5), "b"),
-                                           ast::Name(At(1, 10), "c"))),
-                ast::Name(At(1, 14), "d")));
+            ast::BitwiseAnd(At(1, 3), ast::Name(At(1, 1), "a"),
+                            ast::Name(At(1, 5), "b")));
+  // Test that precedence is handled correctly: equality expressions should bind
+  // more tightly.
+  WithSource("a & b == c");
+  EXPECT_EQ(ParseExpression(),
+            ast::BitwiseAnd(At(1, 3), ast::Name(At(1, 1), "a"),
+                            ast::Equal(At(1, 7), ast::Name(At(1, 5), "b"),
+                                       ast::Name(At(1, 10), "c"))));
 }
 
 TEST_F(ParserTest, BitwiseXor) {
-  WithSource("a ^ b & c ^ d");
-  EXPECT_EQ(
-      ParseExpression(),
-      ast::BitwiseXor(
-          At(1, 11),
-          ast::BitwiseXor(At(1, 3), ast::Name(At(1, 1), "a"),
-                          ast::BitwiseAnd(At(1, 7), ast::Name(At(1, 5), "b"),
-                                          ast::Name(At(1, 9), "c"))),
-          ast::Name(At(1, 13), "d")));
+  WithSource("a ^ b");
+  EXPECT_EQ(ParseExpression(),
+            ast::BitwiseXor(At(1, 3), ast::Name(At(1, 1), "a"),
+                            ast::Name(At(1, 5), "b")));
+  // Test that precedence is handled correctly: bitwise and expressions should
+  // bind more tightly.
+  WithSource("a ^ b & c");
+  EXPECT_EQ(ParseExpression(),
+            ast::BitwiseXor(At(1, 3), ast::Name(At(1, 1), "a"),
+                            ast::BitwiseAnd(At(1, 7), ast::Name(At(1, 5), "b"),
+                                            ast::Name(At(1, 9), "c"))));
 }
 
 TEST_F(ParserTest, BitwiseOr) {
-  WithSource("a | b ^ c | d");
-  EXPECT_EQ(
-      ParseExpression(),
-      ast::BitwiseOr(
-          At(1, 11),
-          ast::BitwiseOr(At(1, 3), ast::Name(At(1, 1), "a"),
-                         ast::BitwiseXor(At(1, 7), ast::Name(At(1, 5), "b"),
-                                         ast::Name(At(1, 9), "c"))),
-          ast::Name(At(1, 13), "d")));
+  WithSource("a | b");
+  EXPECT_EQ(ParseExpression(),
+            ast::BitwiseOr(At(1, 3), ast::Name(At(1, 1), "a"),
+                           ast::Name(At(1, 5), "b")));
+  // Test that precedence is handled correctly: bitwise xor expressions should
+  // bind more tightly.
+  WithSource("a | b ^ c");
+  EXPECT_EQ(ParseExpression(),
+            ast::BitwiseOr(At(1, 3), ast::Name(At(1, 1), "a"),
+                           ast::BitwiseXor(At(1, 7), ast::Name(At(1, 5), "b"),
+                                           ast::Name(At(1, 9), "c"))));
 }
 
 TEST_F(ParserTest, Conjunction) {
-  WithSource("a && b | c && d");
-  EXPECT_EQ(
-      ParseExpression(),
-      ast::LogicalAnd(
-          At(1, 12),
-          ast::LogicalAnd(At(1, 3), ast::Name(At(1, 1), "a"),
-                          ast::BitwiseOr(At(1, 8), ast::Name(At(1, 6), "b"),
-                                         ast::Name(At(1, 10), "c"))),
-          ast::Name(At(1, 15), "d")));
+  WithSource("a && b");
+  EXPECT_EQ(ParseExpression(),
+            ast::LogicalAnd(At(1, 3), ast::Name(At(1, 1), "a"),
+                            ast::Name(At(1, 6), "b")));
+  // Test that precedence is handled correctly: bitwise or expressions should
+  // bind more tightly.
+  WithSource("a && b | c");
+  EXPECT_EQ(ParseExpression(),
+            ast::LogicalAnd(At(1, 3), ast::Name(At(1, 1), "a"),
+                            ast::BitwiseOr(At(1, 8), ast::Name(At(1, 6), "b"),
+                                           ast::Name(At(1, 10), "c"))));
 }
 
 TEST_F(ParserTest, Disjunction) {
-  WithSource("a || b && c || d");
-  EXPECT_EQ(
-      ParseExpression(),
-      ast::LogicalOr(
-          At(1, 13),
-          ast::LogicalOr(At(1, 3), ast::Name(At(1, 1), "a"),
-                         ast::LogicalAnd(At(1, 8), ast::Name(At(1, 6), "b"),
-                                         ast::Name(At(1, 11), "c"))),
-          ast::Name(At(1, 16), "d")));
+  WithSource("a || b");
+  EXPECT_EQ(ParseExpression(),
+            ast::LogicalOr(At(1, 3), ast::Name(At(1, 1), "a"),
+                           ast::Name(At(1, 6), "b")));
+  // Test that precedence is handled correctly: logical and expressions should
+  // bind more tightly.
+  WithSource("a || b && c");
+  EXPECT_EQ(ParseExpression(),
+            ast::LogicalOr(At(1, 3), ast::Name(At(1, 1), "a"),
+                           ast::LogicalAnd(At(1, 8), ast::Name(At(1, 6), "b"),
+                                           ast::Name(At(1, 11), "c"))));
 }
 
 TEST_F(ParserTest, Ternary) {
-  WithSource(
-      "a ?\n"
-      "b ? c : d :\n"
-      "e ? f : g");
+  WithSource("a ? b : c");
+  EXPECT_EQ(ParseExpression(),
+            ast::TernaryExpression(At(1, 3), ast::Name(At(1, 1), "a"),
+                                   ast::Name(At(1, 5), "b"),
+                                   ast::Name(At(1, 9), "c")));
+  // Ternary expression as the first argument of a ternary expression.
+  WithSource("c1 ? c2 ? v1 : v2\n"
+             "   : v3");
   EXPECT_EQ(ParseExpression(),
             ast::TernaryExpression(
-                At(1, 3), ast::Name(At(1, 1), "a"),
-                ast::TernaryExpression(At(2, 3), ast::Name(At(2, 1), "b"),
-                                       ast::Name(At(2, 5), "c"),
-                                       ast::Name(At(2, 9), "d")),
-                ast::TernaryExpression(At(3, 3), ast::Name(At(3, 1), "e"),
-                                       ast::Name(At(3, 5), "f"),
-                                       ast::Name(At(3, 9), "g"))));
+                At(1, 4), ast::Name(At(1, 1), "c1"),
+                ast::TernaryExpression(At(1, 9), ast::Name(At(1, 6), "c2"),
+                                       ast::Name(At(1, 11), "v1"),
+                                       ast::Name(At(1, 16), "v2")),
+                ast::Name(At(2, 6), "v3")));
+  // Ternary expression as the second argument of a ternary expression.
+  WithSource(
+      "c1 ? v1 :\n"
+      "c2 ? v2 :\n"
+      "v3");
+  EXPECT_EQ(ParseExpression(),
+            ast::TernaryExpression(
+                At(1, 4), ast::Name(At(1, 1), "c1"), ast::Name(At(1, 6), "v1"),
+                ast::TernaryExpression(At(2, 4), ast::Name(At(2, 1), "c2"),
+                                       ast::Name(At(2, 6), "v2"),
+                                       ast::Name(At(3, 1), "v3"))));
 }
 
 }  // namespace
