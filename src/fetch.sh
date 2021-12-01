696a60401eaddd04ed36d3c8568157d6034cea28
Joe Fowler <scrumplesplunge@gmail.com>
2021-12-01 18:43:27 +0000

    Add a script for fetching inputs from the website.

---
+++ src/fetch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+day="${1?}"
+if ! [[ -f .cookie ]]; then
+  >&2 echo 'no cookie'
+exit 1
+fi
+cookie="Cookie: $(cat .cookie)"
+directory="$(printf 'puzzles/day%02d' "$day")"
+file="$directory/$USER.input"
+mkdir -p "$directory"
+curl -H "$cookie" "https://adventofcode.com/2020/day/$day/input" > "$file"
+touch "$directory/$USER.output"
